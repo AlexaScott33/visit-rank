fba98d0c551e3f989498691abeee1f806e9e1dd3
Alexa Scott <arscott4792@gmail.com>
2018-08-27 13:34:18 -0700

    tried combining reducers

--- src/store.js
+++ src/store.js
@@ -1,8 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import visitsReducer from './reducers/visits';
+// import savedReducer from './reducers/saved';
+
+// export default createStore(
+//     combineReducers({
+//         visitsReducer,
+//         savedReducer
+//     }),
+//     applyMiddleware(thunk)
+// );
 
 export default createStore(
-    visitsReducer,
+        visitsReducer,
     applyMiddleware(thunk)
 );
