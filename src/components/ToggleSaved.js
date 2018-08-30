f0a9e4f8a6b26bb3a4242308e47a8229bc6d24cc
Alexa Scott <arscott4792@gmail.com>
2018-08-30 14:31:05 -0700

    working on toggleSaved comp

--- src/components/ToggleSaved.js
+++ src/components/ToggleSaved.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { toggleSaved } from '../actions/visits';
 
 class ToggleSaved extends Component {
+    handleToggle() {
+        console.log('toggling!');
+        this.props.dispatch(toggleSaved());
+        console.log('LOOK HERE', this.props);
+    }
     render() {
         return(
             <div>
                 <button
-                onClick={() => console.log('need to toggle')}>Check out your saved searches!</button>
+                onClick={() => this.handleToggle()}>Check out your saved searches!</button>
             </div>
         );
     }
 }
 
-export default ToggleSaved;
+const mapStateToProps = (state) => ({
+    visits: state.visits,
+    saved: state.saved,
+    toggled: state.toggled,
+    loading: state.loading,
+    error: state.error
+});
+
+export default connect(mapStateToProps)(ToggleSaved);
